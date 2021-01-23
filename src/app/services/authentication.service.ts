c737277eb3e698da1b0db68d125541da39ce8d16
Alencruser <alencruserxx@hotmail.fr>
2021-01-23 18:23:11 +0100

    import http to prepare the future calls to backend

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  
+  private http; 
 
-  constructor() { }
+  constructor() {
+    this.http = HttpClient;
+   }
+
+   register(){
+    
+   }
+
 }
