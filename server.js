4c395dbdd9c865db38e096b5dc4e8a7e5b7260a8
Alencruser <alencruserxx@hotmail.fr>
2021-01-23 19:06:00 +0100

    get secured token for session and database

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require('express'),
-    app = express();
+    app = express(),
+    session = require('express-session'),
+    mysql = require('mysql2'),
+    secure = require('./secure.js'),
+    connect = secure.connection,
+    sessionKey = secure.sessionKey,
+    connection = mysql.createConnection(connection);
 
+app.post('/register',()=>{
+
+});
+
+app.post('/login',()=>{
+
+});
+
+app.get('/logout',()=>{
+    // destroy la session id concernée
+});
 
 app.listen('8080', () => {
     console.log('server listening on port 8080')
