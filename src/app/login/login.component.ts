be91f65b143fddee35e0ab4cc223201106cfbd9c
Alencruser <alencruserxx@hotmail.fr>
2021-01-24 16:29:33 +0100

    prepared var and the service function

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public breadcrumb;
+  public username;
+  public pass;
+
+  constructor(private route:ActivatedRoute,private auth:AuthenticationService) {
+    this.breadcrumb = this.route.snapshot.data['breadcrumb'];
+    this.username = "";
+    this.pass = "";
+   }
 
   ngOnInit(): void {
+  }
+
+  register(){
+    if(this.username.length>2 && this.pass.length>7){
+      // Appel service register
+      this.auth.register({
+        username:this.username,
+        password:this.pass
+      })
+    }
+  }
+
+  login(){
+    if(this.username.length>2 && this.pass.length>7){
+      // Appel service register
+      this.auth.login({
+        username:this.username,
+        password:this.pass
+      })
+    }
   }
 
 }
