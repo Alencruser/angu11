8b94e2a8a081aea0e5b8d3d6f076d0d1d6af4325
Alencruser <alencruserxx@hotmail.fr>
2021-01-25 11:42:19 +0100

    removed authguard component, need a service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ArticlesComponent } from './articles/articles.component';
-import { AuthGuardComponent } from './login/authGuard.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
@@ -22,7 +21,7 @@ const routes: Routes = [
   {
     path:'create',
     component:ArticlesComponent,
-    canActivate: [AuthGuardComponent]
+    //canActivate: []
   },
   {
     path:'**',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { AuthGuardComponent } from './login/authGuard.component';
 import { AuthenticationService } from './services/authentication.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
@@ -14,7 +13,6 @@ import { ArticlesComponent } from './articles/articles.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    AuthGuardComponent,
     ArticlesComponent
   ],
   imports: [
